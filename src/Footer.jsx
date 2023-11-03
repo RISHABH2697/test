019341e2e0251ab2cec8be5a14e7aa68cd1958d9
Rishabh Shabha <rishabhsharma.rs1747@gmail.com>
2023-11-03 17:03:48 +0530

    test commit

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Footer = () => {
+
+    const year = new Date().getFullYear();
+    return (
+        <>
+        <footer>
+            <p1>Thank you </p1>
+        </footer>
+        </>
+    );
+  
+};
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const Header = () =>{
+  return(
+        <>
+        <div className="header">
+            <img src="Images/R.png"/>
+            <h1>Sharma keep</h1>
+            
+        </div>
+        </>
+  );
+};
+
+export default Header;
